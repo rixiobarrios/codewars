3e7b42646cdc8b35a7e44e111508edd9492c5c00
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-06-04 09:53:33 -0500

    Update file

--- script.js
+++ script.js
@@ -1181,3 +1181,59 @@
 //         return 'Hello, ' + name + '!';
 //     };
 // }
+
+// Switch/Case - Bug Fixing #6
+
+// Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
+
+// function evalObject(value) {
+//     var result = 0;
+//     switch (value.operation) {
+//         case '+':
+//             result = value.a + value.b;
+//         case '-':
+//             result = value.a - value.b;
+//         case '/':
+//             result = value.a / value.b;
+//         case '*':
+//             result = value.a * value.b;
+//         case '%':
+//             result = value.a % value.b;
+//         case '^':
+//             result = Math.pow(value.a, value.b);
+//     }
+//     return result;
+// }
+
+// Solution:
+
+// function evalObject(value) {
+//     var result = 0;
+//     switch (value.operation) {
+//         case '+':
+//             result = value.a + value.b;
+//             break;
+//         case '-':
+//             result = value.a - value.b;
+//             break;
+//         case '/':
+//             result = value.a / value.b;
+//             break;
+//         case '*':
+//             result = value.a * value.b;
+//             break;
+//         case '%':
+//             result = value.a % value.b;
+//             break;
+//         case '^':
+//             result = Math.pow(value.a, value.b);
+//             break;
+//     }
+//     return result;
+// }
+
+// console.log(evalObject({ a: 1, b: 1, operation: '-' }));
+// console.log(evalObject({ a: 1, b: 1, operation: '/' }));
+// console.log(evalObject({ a: 1, b: 1, operation: '*' }));
+// console.log(evalObject({ a: 1, b: 1, operation: '%' }));
+// console.log(evalObject({ a: 1, b: 1, operation: '^' }));
