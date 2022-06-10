dd5fda82b56ad0bbeb03048833bce8b6273cbcc6
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-06-10 11:50:52 -0500

    Update file

--- script.js
+++ script.js
@@ -1237,3 +1237,56 @@
 // console.log(evalObject({ a: 1, b: 1, operation: '*' }));
 // console.log(evalObject({ a: 1, b: 1, operation: '%' }));
 // console.log(evalObject({ a: 1, b: 1, operation: '^' }));
+
+// Barking mad
+
+// Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+// snoopy.bark(); // return "Woof"
+// scoobydoo.bark(); // undefined
+
+// Use method prototypes to enable all Dogs to bark.
+
+// function Dog(breed) {
+//     this.breed = breed;
+// }
+
+// var snoopy = new Dog('Beagle');
+
+// snoopy.bark = function () {
+//     return 'Woof';
+// };
+
+// var scoobydoo = new Dog('Great Dane');
+
+// Solution:
+
+// function Dog(breed) {
+//     this.breed = breed;
+// }
+
+// var snoopy = new Dog('Beagle');
+
+// snoopy.bark = function () {
+//     return 'Woof';
+// };
+
+// var scoobydoo = new Dog('Great Dane');
+
+// scoobydoo.bark = function () {
+//     return 'Woof';
+// };
+
+// // Other Solution:
+
+// function Dog(breed) {
+//     this.breed = breed;
+// }
+
+// var snoopy = new Dog('Beagle');
+
+// Dog.prototype.bark = function () {
+//     return 'Woof';
+// };
+
+// var scoobydoo = new Dog('Great Dane');
